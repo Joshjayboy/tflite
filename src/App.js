978f1b4dad48e04e31a4fbb0662d8cde7a0a46fb
Akintemi Joshua <jakintemi@gmail.com>
2023-02-18 10:58:40 +0100

    new

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import "./App.css";
 import tflite from "./mobilenet_v1_1.0_224_quant.tflite";
-import React, { useState, useEffect } from "react";
+import wasm from "./tflite.wasm";
+import createTFLiteModel from "./tflite.js";
+import createTFLiteSIMDModel from "./tflite-simd.wasm";
+// import createTFLiteModel from "./tflite-simd.wasm";
 
 console.log(tflite);
 
+
+console.log("createTFLiteModel: ", createTFLiteModel);
+console.log("createTFLiteSIMDModel: ", createTFLiteSIMDModel);
+ console.log(wasm)
+
 const App = () => {
-  return console.log(tflite);
+  return;
+
+  console.log(tflite);
+
+  // console.log(tflite - simd.js);
 };
 
 export default App;
